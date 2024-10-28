2cf6934d5da004180277c72487a603f1466345db
Greg Weber <gweber@digitalmint.io>
2024-10-28 15:45:15 -0500

    return []error rather than error
    Concurrent functions return multiple errors.
    These are now returned only as either a channel or a slice.
    
    This avoids a dependency on a 3rd party multi errors library.
    The standard library errors.Join is awkward to use because
    there is not a simple way to unwrap the errors.
    
    We could use a custom multi error.
    However, to make this convenient to use it should be returned as a
    pointer to the struct rather than an opaque error.
    But Go has a subtle design issue where
    an interface can end up being non-nil when storing a pointer to a struct.
    
    Returning []error still allows the caller to use errors.Join() if they don't
    care about iterating the individual errors.
    
    The main issue is that someone may check != nil rather than len > 0
    However, all the APIs are designed to return nil rather than a zero
    length slice.
    
    Additionally, don't use the default recovery handler.
    Write more errors to the error channels.
    Panic a few errors that shouldn't happen.

--- parallel_test.go
+++ parallel_test.go
@@ -1,7 +1,7 @@
 package parallel_test
 
 import (
-	"fmt"
+	"errors"
 	"sync"
 	"testing"
 
@@ -10,51 +10,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRecoveredCall(t *testing.T) {
-	err := recovery.Call(func() error {
-		return nil
-	})
-	assert.Nil(t, err)
-	err = recovery.Call(func() error {
-		return fmt.Errorf("return error")
-	})
-	assert.NotNil(t, err)
-	err = recovery.Call(func() error {
-		panic("panic")
-	})
-	assert.NotNil(t, err)
-	assert.Equal(t, "panic", err.Error())
-}
-
-func TestGoRecovered(t *testing.T) {
-	noError := func(err error) {
-		assert.Nil(t, err)
-	}
-	errHappened := func(err error) {
-		assert.NotNil(t, err)
-	}
-	recovery.GoHandler(noError, func() error {
-		return nil
-	})
-	recovery.GoHandler(errHappened, func() error {
-		panic("panic")
-	})
-
-	wait := make(chan struct{})
-	go recovery.GoHandler(noError, func() error {
-		wait <- struct{}{}
-		return nil
-	})
-	go recovery.GoHandler(errHappened, func() error {
-		defer func() { wait <- struct{}{} }()
-		panic("panic")
-	})
-	<-wait
-	<-wait
-}
-
 func TestConcurrent(t *testing.T) {
-	var err error
+	var err []error
 	workNone := func(_ int) error { return nil }
 	err = parallel.Concurrent(0, workNone)
 	assert.Nil(t, err)
@@ -76,71 +33,128 @@ func TestConcurrent(t *testing.T) {
 }
 
 func TestQueueWorkers(t *testing.T) {
-	var err error
-	var queue chan int
 	workNone := func(_ int) error { return nil }
-	queue = make(chan int)
-	err = parallel.CollectErrors(parallel.QueueWorkers(0, queue, workNone))
-	assert.Nil(t, err)
-	close(queue)
-
-	queue = make(chan int)
-	go recovery.Go(func() error {
-		queue <- 1
-		queue <- 1
+	{
+		queue := make(chan int)
 		close(queue)
-		return nil
-	})
-	err = parallel.CollectErrors(parallel.QueueWorkers(2, queue, workNone))
-	assert.Nil(t, err)
+		err := parallel.CollectErrors(parallel.QueueWorkers(0, queue, workNone))
+		assert.Nil(t, err)
+	}
 
-	tracked := make([]bool, 10)
-	workTracked := func(i int) error { tracked[i] = true; return nil }
-	queue = make(chan int)
-	go recovery.Go(func() error {
-		queue <- 0
-		queue <- 1
-		close(queue)
-		return nil
-	})
-	err = parallel.CollectErrors(parallel.QueueWorkers(2, queue, workTracked))
-	assert.Nil(t, err)
-	assert.False(t, tracked[2])
-	assert.True(t, tracked[1])
-	assert.True(t, tracked[0])
+	{
+		queue := make(chan int)
+		go recovery.Go(func() error {
+			queue <- 1
+			queue <- 1
+			close(queue)
+			return nil
+		})
+		err := parallel.CollectErrors(parallel.QueueWorkers(2, queue, workNone))
+		assert.Nil(t, err)
+	}
+
+	{
+		tracked := make([]bool, 10)
+		workTracked := func(i int) error { tracked[i] = true; return nil }
+		queue := make(chan int)
+		go recovery.Go(func() error {
+			queue <- 0
+			queue <- 1
+			close(queue)
+			return nil
+		})
+		err := parallel.CollectErrors(parallel.QueueWorkers(2, queue, workTracked))
+		assert.Nil(t, err)
+		assert.False(t, tracked[2])
+		assert.True(t, tracked[1])
+		assert.True(t, tracked[0])
+	}
 }
 
-func arrayWorkers1[T any](nParallel int, objects []T, worker func(int, T) error) error {
+func arrayWorkers1[T any](nParallel int, objects []T, worker func(int, T) error) []error {
 	cancel := make(chan struct{})
 	errors := parallel.ArrayWorkers1(nParallel, objects, cancel, worker)
 	return parallel.CancelAfterFirstError(cancel, errors)
 }
 
+func TestCancelAfterFirstError(t *testing.T) {
+	cancel := make(chan struct{}, 10)
+	{
+		errChan := make(chan error, 10)
+		close(errChan)
+		errs := parallel.CancelAfterFirstError(cancel, errChan)
+		assert.Nil(t, errs)
+	}
+
+	{
+		errChan := make(chan error, 10)
+		errChan <- errors.New("first error")
+		errChan <- errors.New("second error")
+		close(errChan)
+		errs := parallel.CancelAfterFirstError(cancel, errChan)
+		assert.Len(t, errs, 2)
+	}
+}
+
+func TestChannelMerge(t *testing.T) {
+	{
+		c1 := make(chan error)
+		c2 := make(chan error)
+		close(c1)
+		close(c2)
+		err, ok := parallel.TryRecv(parallel.ChannelMerge(c1, c2))
+		assert.False(t, ok)
+		assert.Nil(t, err)
+	}
+
+	{
+		c1 := make(chan error)
+		c2 := make(chan error)
+		go func() {
+			c1 <- errors.New("c1")
+			c2 <- errors.New("c2")
+			close(c1)
+			close(c2)
+		}()
+		merged := parallel.ChannelMerge(c1, c2)
+		_, ok := <-merged
+		assert.True(t, ok)
+		_, ok = <-merged
+		assert.True(t, ok)
+		_, ok = <-merged
+		assert.False(t, ok)
+	}
+}
+
 func TestArrayWorkers1(t *testing.T) {
-	var err error
 	workNone := func(_ int, _ bool) error { return nil }
-	tracked := make([]bool, 10)
-	err = arrayWorkers1(0, tracked, workNone)
-	assert.Nil(t, err)
+	{
+		tracked := make([]bool, 10)
+		err := arrayWorkers1(0, tracked, workNone)
+		assert.Nil(t, err)
+	}
 
-	tracked = make([]bool, 1)
-	err = arrayWorkers1(10, tracked, workNone)
-	assert.Nil(t, err)
+	{
+		tracked := make([]bool, 1)
+		err := arrayWorkers1(10, tracked, workNone)
+		assert.Nil(t, err)
+	}
 
-	tracked = make([]bool, 10)
-	workTracked := func(i int, _ bool) error { tracked[i] = true; return nil }
-	err = arrayWorkers1(0, tracked, workTracked)
-	assert.Nil(t, err)
-	assert.False(t, tracked[0])
+	{
+		tracked := make([]bool, 10)
+		workTracked := func(i int, _ bool) error { tracked[i] = true; return nil }
+		err := arrayWorkers1(1, tracked, workTracked)
+		assert.Nil(t, err)
+		assert.NotContains(t, tracked, false)
+	}
 
-	tracked = make([]bool, 10)
-	workTracked = func(i int, _ bool) error { tracked[i] = true; return nil }
-	err = arrayWorkers1(2, tracked, workTracked)
-	assert.Nil(t, err)
-	assert.True(t, tracked[0])
-	assert.True(t, tracked[1])
-	assert.True(t, tracked[2])
-	assert.True(t, tracked[9])
+	{
+		tracked := make([]bool, 10)
+		workTracked := func(i int, _ bool) error { tracked[i] = true; return nil }
+		err := arrayWorkers1(2, tracked, workTracked)
+		assert.Nil(t, err)
+		assert.NotContains(t, tracked, false)
+	}
 }
 
 type SyncNumber struct {
@@ -155,22 +169,21 @@ func (sn *SyncNumber) Add(x int) {
 }
 
 func TestBatchWorkers(t *testing.T) {
-	var err error
 	workNone := func(_ []bool) error { return nil }
-	tracked := make([]bool, 10)
-	bw := parallel.BatchWork{Size: 2, Parallelism: 0}
-	err = parallel.BatchWorkers(bw, tracked, workNone)
-	assert.Nil(t, err)
-
-	tracked = make([]bool, 10)
-	bw = parallel.BatchWork{Size: 2, Parallelism: 2}
-	err = parallel.BatchWorkers(bw, tracked, workNone)
-	assert.Nil(t, err)
-
-	work := make([]int, 10)
-	for i := range work {
-		work[i] = i + 1
+	{
+		tracked := make([]bool, 10)
+		bw := parallel.BatchWork{Size: 2, Parallelism: 0}
+		err := parallel.BatchWorkers(bw, tracked, workNone)
+		assert.Nil(t, err)
 	}
+
+	{
+		tracked := make([]bool, 10)
+		bw := parallel.BatchWork{Size: 2, Parallelism: 2}
+		err := parallel.BatchWorkers(bw, tracked, workNone)
+		assert.Nil(t, err)
+	}
+
 	output := SyncNumber{Number: 0}
 	add := func(batch []int) error {
 		for _, x := range batch {
@@ -178,22 +191,34 @@ func TestBatchWorkers(t *testing.T) {
 		}
 		return nil
 	}
-	bw = parallel.BatchWork{Size: 1, Parallelism: 1}
-	err = parallel.BatchWorkers(bw, work, add)
-	assert.Nil(t, err)
-	assert.Equal(t, output.Number, 55)
+	work := make([]int, 10)
+	for i := range work {
+		work[i] = i + 1
+	}
 
-	output = SyncNumber{Number: 0}
-	bw = parallel.BatchWork{Size: 2, Parallelism: 2}
-	err = parallel.BatchWorkers(bw, work, add)
-	assert.Nil(t, err)
-	assert.Equal(t, output.Number, 55)
+	{
+		output = SyncNumber{Number: 0}
+		bw := parallel.BatchWork{Size: 1, Parallelism: 1}
+		err := parallel.BatchWorkers(bw, work, add)
+		assert.Nil(t, err)
+		assert.Equal(t, output.Number, 55)
+	}
 
-	output = SyncNumber{Number: 0}
-	bw = parallel.BatchWork{Size: 3, Parallelism: 3}
-	err = parallel.BatchWorkers(bw, work, add)
-	assert.Nil(t, err)
-	assert.Equal(t, output.Number, 55)
+	{
+		output = SyncNumber{Number: 0}
+		bw := parallel.BatchWork{Size: 2, Parallelism: 2}
+		err := parallel.BatchWorkers(bw, work, add)
+		assert.Nil(t, err)
+		assert.Equal(t, output.Number, 55)
+	}
+
+	{
+		output = SyncNumber{Number: 0}
+		bw := parallel.BatchWork{Size: 3, Parallelism: 3}
+		err := parallel.BatchWorkers(bw, work, add)
+		assert.Nil(t, err)
+		assert.Equal(t, output.Number, 55)
+	}
 }
 
 func TestBatchWorkersSmallBatchSize(t *testing.T) {
